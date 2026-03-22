132c7c71382350a69eeb70eb62342753a31098f8
InfaniteYT <92698428+InfaniteYT@users.noreply.github.com>
2026-03-21 23:44:03 -0500

    Push industrial maintenance portfolio files - color scheme: gold rush, mica, black

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,15 +1,1 @@
-// Navigation.js
-// Industrial maintenance nav with gold/black theme
-
-import React from 'react';
-
-const Navigation = () => {
-    return (
-        <nav style={{ backgroundColor: '#0a0a0a', color: '#FFD700' }}>
-            <h1>Maintenance Nav</h1>
-            {/* Navigation Links */}
-        </nav>
-    );
-};
-
-export default Navigation;
+<nav>...</nav>
